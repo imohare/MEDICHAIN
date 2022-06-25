b296c05b03c69500ac80f63704510db0b66f7380
aoniatraxler <traxler.aonia@gmail.com>
2022-06-25 18:03:31 -0400

    added privy data

--- client/my-app/src/pages/patient/patient-data.jsx
+++ client/my-app/src/pages/patient/patient-data.jsx
@@ -1,15 +1,28 @@
 import { Flex, Box, Text } from "rebass";
-
+import { useEffect, useState } from 'react'
+import { PrivyClient, SiweSession } from '@privy-io/privy-browser'
 import LogoutButton from "../../buttons/logout";
+
+// Initialize the Privy client.
+const provider = typeof window !== "undefined" ? window.ethereum : null;
+const session = new SiweSession(process.env.NEXT_PUBLIC_PRIVY_API_KEY, provider)
+const client = new PrivyClient({
+  session: session,
+});
+
+
+const userId = await session.address();
 
 export default function PatientData() {
 
+
+
   return (
-   <div>
-    <Flex>
-      <LogoutButton/>
-    </Flex>
-     <text>PatientData</text>
-   </div>
+    <div>
+      <Flex>
+        <LogoutButton />
+      </Flex>
+      <text>PatientData</text>
+    </div>
   )
 }
