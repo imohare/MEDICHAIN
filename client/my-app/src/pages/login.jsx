78bfe47247e6e3947544d851fda41c5e239737f9
aoniatraxler <traxler.aonia@gmail.com>
2022-06-25 12:45:20 -0400

    added login functionality

--- client/my-app/src/pages/login.jsx
+++ client/my-app/src/pages/login.jsx
@@ -1,11 +1,47 @@
 // import LogoutButton from '../../buttons/logout';
 // import { Flex, Box, Text } from "rebass";
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+const { ethers } = require("ethers");
 
- export default function Login() {
 
-   return (
+
+export default function Login() {
+  const [ethAddress, setEthAddress] = useState('No address yet')
+  const loginHandler = async () => {
+    try {
+      if (window.ethereum) {
+        //metamask is here
+        // window.ethereum.request({ method: 'eth_requestAccounts' })
+        //   .then(result => {
+        //     setEthAddress(result[0]);
+        //     console.log("log in")
+        //     return result[0]
+        //   })
+        //   .then(
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        console.log(provider);
+        // Prompt user for account connections
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        console.log("Account:", await signer.getAddress());
+        //navigate to next page
+
+      }
+    }
+    catch {
+      console.log('Please install Metamask');
+
+    }
+  }
+
+  return (
     <div>
-      <text>Login</text>
+      <h1>Welcome To Medichain</h1>
+      <h2>Take Control Of Your Own Data</h2>
+      <button onClick={loginHandler}>Login</button>
+
+
     </div>
-   )
- }
+  )
+}
