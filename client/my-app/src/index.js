7e57346f745ed0f9dff33bad82fcd4b010ed1aee
aoniatraxler <traxler.aonia@gmail.com>
2022-06-25 16:30:33 -0400

    privy

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddPatient from './pages/doctor/add-patient';
 import AddPatientData from './pages/doctor/add-patient-data';
@@ -7,21 +7,11 @@ import Patients from './pages/doctor/patients';
 import PatientData from './pages/patient/patient-data';
 import PatientPermissions from './pages/patient/patient-permissions';
 import Login from './pages/login';
-=======
- import { BrowserRouter, Routes, Route } from "react-router-dom";
- import AddPatient from './pages/doctor/add-patient';
- import AddPatientData from './pages/doctor/add-patient-data';
- import Patients from './pages/doctor/patients';
- import PatientData from './pages/patient/patient-data';
- import DoctorPatientData from './pages/doctor/patient-data';
- import PatientPermissions from './pages/patient/patient-permissions';
- import Login from './pages/login';
->>>>>>> 19ccb8b7efc508dee63612c27fce8f3de81a1ee6
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-<<<<<<< HEAD
     <Routes>
       <Route path="login" element={<Login />} />
       <Route path="patient" element={<PatientData />} />
@@ -33,16 +23,4 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
-=======
-  <Routes>
-       <Route path="login" element={<Login />} />
-       <Route path="patient" element={<PatientData />} />
-       <Route path="patient/permissions" element={<PatientPermissions />} />
-       <Route path="doctor" element={<Patients />} />
-       <Route path="doctor/:patientId" element={<DoctorPatientData />} />
-       <Route path="doctor/add" element={<AddPatient />} />
-       <Route path="doctor/addData" element={<AddPatientData />} />
-     </Routes>
-     </BrowserRouter>
-     )
->>>>>>> 19ccb8b7efc508dee63612c27fce8f3de81a1ee6
+
--- client/my-app/src/pages/login.jsx
+++ client/my-app/src/pages/login.jsx
@@ -2,23 +2,44 @@
 // import { Flex, Box, Text } from "rebass";
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import "./login.css";
+import { PrivyClient, SiweSession } from '@privy-io/privy-browser'
 const { ethers } = require("ethers");
 
 
 
+
+
 export default function Login() {
-  const [ethAddress, setEthAddress] = useState('No address yet')
+  const [ethAddress, setEthAddress] = useState('No address yet');
+  const [providerState, setProvider] = useState('No provider yet');
+  const [signerState, setSigner] = useState('No signer yet');
+  const [sessionState, setSession] = useState('No session yet');
+  const [clientState, setClient] = useState('No client yet');
+
+
   const loginHandler = async () => {
     try {
       if (window.ethereum) {
         const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        setProvider(provider);
         console.log(provider);
         // Prompt user for account connections
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
+        setSigner(signer);
         console.log("Account:", await signer.getAddress());
         //navigate to next page
 
+        //const provider = typeof window !== "undefined" ? window.ethereum : null;
+        const session = new SiweSession(process.env.NEXT_PUBLIC_PRIVY_API_KEY, window.ethereum)
+        setSession(session);
+        const client = new PrivyClient({
+          session: session,
+        });
+        setClient(client);
+
+        //iitialise privy client
       }
     }
     catch {
