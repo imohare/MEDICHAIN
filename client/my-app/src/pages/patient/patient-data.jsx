7dcf5b21c48f4d5029d67489a8ed0569691e86d2
cenbiliu <cenbiliu@gmail.com>
2022-06-25 14:57:59 -0400

    read in simple json on /patient page

--- client/my-app/src/pages/patient/patient-data.jsx
+++ client/my-app/src/pages/patient/patient-data.jsx
@@ -1,15 +1,23 @@
-import { Flex, Box, Text } from "rebass";
-
+import { Flex, Box, Text} from "rebass";
 import LogoutButton from "../../buttons/logout";
+import myData from './medicationAdmin.json';
+
 
 export default function PatientData() {
-
+  var arr = [];
+  Object.keys(myData).forEach(function(key) {
+    arr.push(myData[key]);
+  });
+  const listItems = arr.map((field) =>
+  <li>{field}</li>
+  );
   return (
    <div>
     <Flex>
       <LogoutButton/>
     </Flex>
-     <text>PatientData</text>
+    <text>PatientData</text>
+    <ul>{listItems}</ul>
    </div>
   )
 }
